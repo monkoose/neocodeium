666d4f57e8bf0789c58957a9483dd671bc038123
monkoose <pythonproof@gmail.com>
2024-05-04 12:40:04 +0300

    Bump server binary version to 1.8.31

--- lua/neocodeium/binary.lua
+++ lua/neocodeium/binary.lua
@@ -15,7 +15,7 @@ local fn = vim.fn
 ---@field path filepath
 ---@field suffix? string
 ---@field sha? string
-local Bin = { version = "1.8.30" }
+local Bin = { version = "1.8.31" }
 
 -- Auxiliary functions ------------------------------------- {{{1
 
