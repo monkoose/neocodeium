c0d5d45e8acc5168de8c163bcc3bb041588e35f3
monkoose <pythonproof@gmail.com>
2025-05-04 17:10:44 +0300

    Use string.rep instead of string.sub
    Speed gain is neglectable, but improves readability a lot and works for
    any number of tabs.

--- lua/neocodeium/completer.lua
+++ lua/neocodeium/completer.lua
@@ -121,15 +121,10 @@ end
 ---@param str string
 ---@return string
 local function leading_tabs_to_spaces(str)
-   ---@diagnostic disable-next-line: redundant-return-value
-   return str:gsub("^\t*", function(m)
-      -- faster than string.rep
-      return string.sub(
-         [[                                                         ]],
-         1,
-         #m * fn.shiftwidth()
-      )
+   str = str:gsub("^\t*", function(m)
+      return string.rep(" ", #m * fn.shiftwidth())
    end)
+   return str
 end
 
 ---Adds virtual text below the line with `lnum` number.
