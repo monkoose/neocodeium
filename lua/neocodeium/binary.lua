3115581bdf85d0eae4cc8b9b8b1bd3c5d5c8e1cf
monkoose <pythonproof@gmail.com>
2025-05-18 21:10:08 +0300

    Refactor binary module

--- lua/neocodeium/binary.lua
+++ lua/neocodeium/binary.lua
@@ -14,13 +14,25 @@ local fs = vim.fs
 ---@class Binary
 ---@field version string
 ---@field path filepath
----@field suffix? string
+---@field name? string
 local Bin = { version = "1.46.3" }
 
 -- Auxiliary functions ------------------------------------- {{{1
 
+---Checks if binary was expanded successfully.
+---@return boolean
+local function is_expanded_successfully()
+   if vim.v.shell_error ~= 0 then
+      log.error("Failed to extract binary\n" .. vim.v.shell_error, { type = log.BOTH })
+      return false
+   end
+   return true
+end
+
 ---Expands language server binary from compressed file on Windows.
+---Returns `true` if expansion was successful.
 ---@param bin_gz filepath
+---@return boolean
 local function powershell_expand(bin_gz)
    utils.with_shell(function()
       vim.o.shell = "powershell"
@@ -36,6 +48,25 @@ local function powershell_expand(bin_gz)
             .. " }"
       )
    end)
+   return is_expanded_successfully()
+end
+
+---Expands language server binary from compressed file on POSIX.
+---Returns `true` if expansion was successful.
+---@param bin_path filepath
+local function posix_expand(bin_path)
+   -- Uncompress binary
+   fn.system("gzip -d " .. fn.shellescape(bin_path .. ".gz"))
+   if not is_expanded_successfully() then
+      return false
+   end
+   -- Make binary executable
+   fn.system("chmod +x " .. fn.shellescape(bin_path))
+   if vim.v.shell_error ~= 0 then
+      log.error("Failed to make binary executable\n" .. vim.v.shell_error, { type = log.BOTH })
+      return false
+   end
+   return true
 end
 
 -- Binary methods ------------------------------------------ {{{1
@@ -46,21 +77,19 @@ function Bin.new()
 
    if options.bin and stdio.readable(options.bin) then
       self.path = options.bin
-   else
-      local system_info = utils.get_system_info()
-      if system_info.os == "unsupported" or system_info.arch == "unsupported" then
-         log.error("Unsupported OS or architecture", { type = log.BOTH })
-      else
-         self.suffix = system_info.os .. "_" .. system_info.arch
-         if system_info.os == "windows" then
-            self.suffix = self.suffix .. ".exe"
-         end
-
-         local bin_dir = conf.dir .. "/bin/" .. self.version
-         self.path = bin_dir .. "/language_server_" .. self.suffix
-      end
+      return self
    end
 
+   local system_info = utils.get_system_info()
+   if system_info.os == "unsupported" or system_info.arch == "unsupported" then
+      log.error("Unsupported OS or architecture", { type = log.BOTH })
+   else
+      self.name = "language_server_" .. system_info.os .. "_" .. system_info.arch
+      if system_info.os == "windows" then
+         self.name = self.name .. ".exe"
+      end
+      self.path = fs.joinpath(conf.dir, "bin", self.version, self.name)
+   end
    return self
 end
 
@@ -76,12 +105,7 @@ function Bin:download(callback)
 
    local base_url = "https://github.com/Exafunction/codeium/releases/download"
    ---@type url
-   local url = string.format(
-      "%s/language-server-v%s/language_server_%s.gz",
-      base_url,
-      self.version,
-      self.suffix
-   )
+   local url = string.format("%s/language-server-v%s/%s.gz", base_url, self.version, self.name)
 
    local bin_dir = fs.dirname(self.path)
    if bin_dir then
@@ -98,9 +122,7 @@ function Bin:download(callback)
       vim.schedule_wrap(function(o)
          if o.code ~= 0 then
             log.error("Failed to download binary\n" .. o.stderr, { type = log.BOTH })
-            return
-         end
-         if callback then
+         elseif callback then
             callback()
          end
       end)
@@ -115,27 +137,22 @@ function Bin:expand()
       log.error("Binary path not set", { type = log.BOTH })
       return false
    end
+
    local bin_gz = self.path .. ".gz"
    log.info("Extracting binary...", { type = log.ECHO })
+
+   local ok
    if utils.get_system_info().os == "windows" then
-      powershell_expand(bin_gz)
+      ok = powershell_expand(bin_gz)
    else
-      -- Uncompress binary
-      fn.system("gzip -d " .. fn.shellescape(bin_gz))
-      if vim.v.shell_error ~= 0 then
-         log.error("Failed to extract binary\n" .. vim.v.shell_error, { type = log.BOTH })
-         return false
-      end
-      -- Make binary executable
-      fn.system("chmod +x " .. fn.shellescape(self.path))
-      if vim.v.shell_error ~= 0 then
-         log.error("Failed to make binary executable\n" .. vim.v.shell_error, { type = log.BOTH })
-         return false
-      else
-         log.info("Binary extracted successfully", { type = log.ECHO })
-      end
+      ok = posix_expand(self.path)
    end
-   return true
+
+   if ok then
+      log.info("Binary extracted successfully", { type = log.ECHO })
+   end
+
+   return ok
 end
 -- }}}1
 
